c333e109f6592040a9a89241f1364fd8788c9a26
Leon <t7oele00@students.oamk.fi>
2019-10-09 14:34:09 +0300

    Tiny cosmetic change
    Added some <hr>

--- iot-weatherstation/src/App.js
+++ iot-weatherstation/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Header from './components/Header.js';
 import LocalWeather from './components/LocalWeather.js';
 import PiWeather from './components/PiWeather.js';
@@ -11,13 +10,9 @@ function App() {
   return (
     <div className="Main">
       <div><Header/></div>
-      <br/>
-      <div><LocalWeather/>
-      <PiWeather/>
-      </div>
-      <div>
-        <Data/>
-      </div>
+      <div><LocalWeather/></div><hr/>
+      <div><PiWeather/></div><hr/>
+      <div><Data/></div><hr/>
       <br/>
       <br/>
       <br/>
