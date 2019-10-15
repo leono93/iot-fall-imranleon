8c432977f146f9bbbb476688606e30b28e3bdc5f
Leon <t7oele00@students.oamk.fi>
2019-10-15 15:09:38 +0300

    Imported values from local database

--- iot-weatherstation/src/components/PiWeather.js
+++ iot-weatherstation/src/components/PiWeather.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Chart from 'react-google-charts';
+import Import from '../components/Import.js';
 
 class PiWeather extends Component {
 
@@ -7,7 +8,7 @@ class PiWeather extends Component {
         return (
             <div className="piweather">
                 <h1>Information from Raspberry Pi Weather Station</h1>
-                  
+               <Import/>
             </div>
         );
     }
