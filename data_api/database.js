0b8f88c126c9d1053feac3d37fa2a3b834666081
Leon <t7oele00@students.oamk.fi>
2019-10-15 14:19:46 +0300

    Removal of unnecessary firebase files

--- data_api/database.js
+++ data_api/database.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 var connection = mysql.createPool({
-  host: '193.167.107.131',
-  port: '22',
+  host: 'localhost',
+  port: '3306',
   user: 'iot-project',
   password: 'oamkiot2019',
   database: 'sensor_vals'
