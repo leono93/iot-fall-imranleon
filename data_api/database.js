b301a4541ce7b90d2be0826ad40de349905824bd
Leon <t7oele00@students.oamk.fi>
2019-10-16 16:06:04 +0300

    Node modules and database
    Removed unignored modules and database configuration file (maybe should have done that earlier)

--- data_api/database.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-var connection = mysql.createPool({
-  host: 'localhost',
-  port: '3306',
-  user: 'iot-project',
-  password: 'oamkiot2019',
-  database: 'sensor_vals'
-});
-module.exports = connection;
